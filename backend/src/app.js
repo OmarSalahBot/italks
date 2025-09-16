908013d4c2f888ea1d42a21ce140e675c42d7c5e
OmarSalahBot <Omarsalah8001@gmail.com>
2025-09-16 09:10:15 +0300

    fixing Problem

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-import notesRoutes from './Routes/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js';
